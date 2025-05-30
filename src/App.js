75070f3745cb09a49008f2bf1a23452cfa6ac2da
Prabhav Giriya <prabhavgiriya925@gmail.com>
2025-05-30 23:17:18 +0530

    created the product page and filters , updating the params in url with selected filters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './customer/component/Navigation/Navigation';
+import Navigation from './customer/component/Navigation/Navigation.jsx';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/component/footer/Footer';
+import Product from './customer/component/Product/Product.jsx';
 
 function App() {
   return (
     <div className="">
       <Navigation />
        <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
        </div>
      <Footer />
     </div>
