5c082cb4094e3d91b31d277bf6e6e1d0f334201c
Prabhav Giriya <prabhavgiriya925@gmail.com>
2025-07-26 21:26:45 +0530

    replaced the localhost backend url with live link

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "myeshop-backend.railway.internal"
 
 const jwt = localStorage.getItem("jwt");
 
