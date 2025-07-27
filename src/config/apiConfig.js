77bf4c1f1e18277dd235b772be0077676e863bfd
Prabhav Giriya <prabhavgiriya925@gmail.com>
2025-07-27 17:56:59 +0530

    fixed the backend url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "myeshop-backend.railway.internal"
+export const API_BASE_URL = "https://myeshop-backend-production.up.railway.app/"
 
 const jwt = localStorage.getItem("jwt");
 
